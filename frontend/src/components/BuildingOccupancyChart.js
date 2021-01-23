113b3b391fd1f4916154390d542fe8641196fb51
cliu55 <y9t0b@ugrad.cs.ubc.ca>
2021-01-22 17:35:42 -0800

    Sort building occupancy chart labels

--- frontend/src/components/BuildingOccupancyChart.js
+++ frontend/src/components/BuildingOccupancyChart.js
@@ -6,19 +6,10 @@ export default function BuildingOccupancyChart({
   buildingMaxOccupy,
 }) {
   const labels = Object.keys(checkInByArea).length
-    ? Object.keys(checkInByArea)
-    : Object.keys(buildingMaxOccupy);
-  let areaCheckIn = Object.values(checkInByArea);
-  let maxOccupancy = areaCheckIn.map((count, idx) =>
-    buildingMaxOccupy[labels[idx]] ? buildingMaxOccupy[labels[idx]] : null
-  );
-  areaCheckIn = areaCheckIn.length
-    ? areaCheckIn
-    : new Array(Object.keys(buildingMaxOccupy).length).fill(0);
-  maxOccupancy = maxOccupancy.length
-    ? maxOccupancy
-    : Object.values(buildingMaxOccupy);
-
+    ? Object.keys(checkInByArea).sort()
+    : Object.keys(buildingMaxOccupy).sort();
+  const areaCheckIn = labels.map(label => checkInByArea[label] ? checkInByArea[label] : 0);
+  const maxOccupancy = labels.map(label => buildingMaxOccupy[label]);
   const data = {
     labels,
     datasets: [
